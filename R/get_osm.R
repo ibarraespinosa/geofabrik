a60de465e39553888fa1cf1b799ac6ae53c7e107
ibarraespinosa <sergio.ibarra@usp.br>
2020-08-20 10:26:52 -0300

    improve documentation

--- R/get_osm.R
+++ R/get_osm.R
@@ -9,16 +9,16 @@
 #' @param quiet If TRUE, suppress status messages (if any), and the progress bar.
 #' @return Download a file
 #' @importFrom utils download.file
+#' @note all regions were transformed with  iconv to "ASCII//TRANSLIT"
 #' @export
-#' @examples \dontrun{
-#' # do not run
+#' @examples \donttest{
 #' head(urlgeo)
 #' get_osm("Sao Tome and Principe")
 #' get_osm("Sao Tome and Principe", "pbf")
 #' get_osm("Sao Tome and Principe", "osm")
 #' }
 get_osm <- function(region,
-                    type = c("pbf", "shp", "osm")[2],
+                    type = "shp",
                     file = tempfile(),
                     quiet = FALSE) {
   URL <- urlgeo[urlgeo$x == region,
@@ -26,7 +26,7 @@ get_osm <- function(region,
                       ifelse(type == "shp", "URL_SHP",
                              "URL_OSMBZ2"))][1] #if repeated URL
   dfile <- ifelse(type == "pbf", paste0(file, ".osm.pbf"),
-                  ifelse(type == "shp", paste0(file, "shp.zip"),
+                  ifelse(type == "shp", paste0(file, ".shp.zip"),
                          paste0(file, ".osm.bz2")))
 download.file(url = URL,
               destfile = dfile,
